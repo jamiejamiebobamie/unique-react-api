68aabafe958ad4449f0941b988981b709bffe1ee
jamiejamiebobamie <jmccrory@vt.edu>
2020-07-31 04:14:03 -0700

    pushing

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
         super(props)
         this.state = {
             isLoading: false,
-            data: "generated text will appear here",
+            data: null,
             inputTextValue: ipsum,
             handleValue: '@Oprah',
             urlValue: 'https://www.gutenberg.org/files/1342/1342-0.txt',
@@ -30,74 +30,16 @@ class App extends Component {
         this.submitHandle = this.submitHandle.bind(this);
         this.submitURL = this.submitURL.bind(this);
         this.submitAuthor = this.submitAuthor.bind(this);
+        this.getRandHandleAndSubmit = this.getRandHandleAndSubmit.bind(this);
+
     }
-
-    // getQuoteFromDB(){
-    //     this.setState({isLoading:true})
-    //     const url = "http://localhost:7000/api/v1/quote"
-    //     // Get data from the API with fetch
-    //     fetch(url).then(res => {
-    //       // Handle the response stream as JSON
-    //       return res.json()
-    //     }).then((json) => {
-    //       this.setState({ data: json.quote })
-    //       this.setState({isLoading:false})
-    //     }).catch((err) => {
-    //       // If there is no data
-    //       this.setState({ data: null })
-    //       this.setState({error:undefined})
-    //
-    //       // Print an error to the console.
-    //       console.log('-- Error fetching --')
-    //       console.log(err.message)
-    //
-    //       // You may want to display an error to the screen here.
-    //           this.setState({isLoading:false})
-    //     })
-    // }
-
-  // handleSubmit(e) {
-  //   this.setState({isLoading:true})
-  //   this.setState({error:undefined})
-  //   e.preventDefault()
-  //   const data = this.getState.inputValue
-  //   const url = "http://localhost:7000/api/v1/quote-from-input"
-  //
-  //   // Get data from the API with fetch
-  //   fetch(url, {
-  //                   method: 'POST',
-  //                   headers: { 'Content-Type': 'application/json'},
-  //                   body: JSON.stringify(data)
-  //               }).then(res => {
-  //     // Handle the response stream as JSON
-  //     return res.json()
-  //   }).then((json) => {
-  //     // If the request was successful assign the data to component state
-  //     console.log(this.state.data)
-  //     // this.setState({ inputValue: json.quote })
-  //     this.setState({ data: json.quote })
-  //     this.setState({error:this.state.data.error})
-  //
-  //     this.setState({isLoading:false})
-  //   }).catch((err) => {
-  //     // If there is no data
-  //     this.setState({ data: null }) // Clear the weather data we don't have any to display
-  //     this.setState({error:undefined})
-  //
-  //     // Print an error to the console.
-  //     console.log('-- Error fetching --')
-  //     console.log(err.message)
-  //
-  //     // You may want to display an error to the screen here.
-  //         this.setState({isLoading:false})
-  //   })
-  // }
 
   submitText(){
     this.setState({isLoading:true})
     this.setState({error:undefined})
     const data = this.state.inputTextValue
-    const backend = "http://localhost:7000/api/v1/quote-from-input"
+    const backend = "https://re-tweet.herokuapp.com/api/v1/quote-from-input"
+
     // Get data from the API with fetch
     fetch(backend, {
                 method: 'POST',
@@ -132,7 +74,8 @@ class App extends Component {
       this.setState({error:undefined})
       const handle = this.state.handleValue
       // const url = "http://www.gutenberg.org/cache/epub/288/pg288.txt"
-      const backend = "http://localhost:7000/api/v1/quote-from-twitter-handle"
+      const backend = 'https://re-tweet.herokuapp.com/api/v1/quote-from-twitter-handle'
+      // const backend = "http://localhost:7000/api/v1/quote-from-twitter-handle"
       // Get data from the API with fetch
       fetch(backend, {
                   method: 'POST',
@@ -166,7 +109,9 @@ class App extends Component {
     this.setState({error:undefined})
     const url = this.state.urlValue
     // const url = "http://www.gutenberg.org/cache/epub/288/pg288.txt"
-    const backend = "http://localhost:7000/api/v1/quote-from-url"
+    // const backend = "http://localhost:7000/api/v1/quote-from-url"
+    const backend = "https://re-tweet.herokuapp.com/api/v1/quote-from-url"
+
     // Get data from the API with fetch
     fetch(backend, {
                 method: 'POST',
@@ -200,7 +145,9 @@ class App extends Component {
       this.setState({error:undefined})
       const author = this.state.authorValue
       // const url = "http://www.gutenberg.org/cache/epub/288/pg288.txt"
-      const backend = "http://localhost:7000/api/v1/quote-from-author"
+      // const backend = "http://localhost:7000/api/v1/quote-from-author"
+      const backend = "https://re-tweet.herokuapp.com/api/v1/quote-from-author"
+
       // Get data from the API with fetch
       fetch(backend, {
                   method: 'POST',
@@ -229,74 +176,111 @@ class App extends Component {
       })
   }
 
+  getRandHandleAndSubmit(){
+      console.log("hey")
+      this.setState({error:undefined})
+      const backend = 'https://re-tweet.herokuapp.com/api/v1/rand-handle'
+      fetch(backend).then(res => {
+        return res.json()
+      }).then((json) => {
+        this.setState({ handleValue: '@' + json.handle })
+      }).then(() => {
+          this.submitHandle();
+      }).catch((err) => {
+        console.log('-- Error fetching --')
+        console.log(err.message)
+        this.setState({isLoading:false})
+      })
+  }
+
     render(){
         return (
           <div className="App">
               <div className="boxes">
-              {
-                  (this.state.isLoading) ?
-                    <div className="output-Box"><Error error={this.state.weatherData} isLoading={this.state.isLoading}/></div> :
-                    <div className="output-Box"> <div className="output">{this.state.data}</div></div>
-            }
-            <div className="output-Box">
-              <div className="input-Box">
-              <Tabs selectedindex={this.state.tabIndex} onSelect={tabIndex => this.setState({ tabIndex })}>
-                <TabList selectedindex={this.state.tabIndex} onSelect={tabIndex => this.setState({ tabIndex })}>
-                    <Tab>text</Tab>
-                    <Tab>tweet</Tab>
-                    <Tab>url</Tab>
-                    <Tab>author</Tab>
-                </TabList>
-                <TabPanel>
-                <div className="input-Box">
-                    <textarea className="input" placeholder='enter text here' value={this.state.inputTextValue}
-                        onChange={e => this.setState({ inputTextValue: e.target.value })}
-                        onSubmit={e => this.handleSubmit(e)}>
-                    </textarea>
-                    <button onClick={this.submitText} type="submit">Submit</button>
+                  <div className="output-Box">
+                  { (this.state.isLoading ) ?
+                        <Error error={null} isLoading={this.state.isLoading}/> :
+                            <div className="output">{this.state.data}</div>
+                  }
+                  {(this.state.isLoading || this.state.data==null) ? "":
+                  <button className="tweetButton" onClick={this.submitHandle} type="submit">Tweet it!</button>}
                 </div>
-                </TabPanel>
-                <TabPanel>
-                <div className="input-Box">
-                    <input className="inputUrl" value={this.state.handleValue}
-                        onChange={e => this.setState({ handleValue: e.target.value })}
-                        onSubmit={e => this.handleSubmit(e)}>
-                    </input>
-                    <button onClick={this.submitHandle} type="submit">Submit</button>
-                </div>
-                </TabPanel>
-                <TabPanel>
                     <div className="input-Box">
-                        <input className="inputUrl" value={this.state.urlValue}
-                            onChange={e => this.setState({ urlValue: e.target.value })}
+                        <input className="inputUrl" value={this.state.handleValue}
+                            onChange={e => this.setState({ handleValue: e.target.value })}
                             onSubmit={e => this.handleSubmit(e)}>
                         </input>
-                        <button onClick={this.submitURL} type="submit">Submit</button>
+                        <div className="buttons">
+                            <button onClick={this.submitHandle} type="submit">Generate</button>
+                            <button onClick={this.getRandHandleAndSubmit} type="submit">Random</button>
+                        </div>
                     </div>
-                </TabPanel>
-                <TabPanel>
-                    <div className="input-Box">
-                        <div>
-                          <input type="radio" name="author" id="grimm" value="grimm" onChange={e => this.setState({ authorValue: e.target.value })}/>
-                          <label htmlFor="grimm">The Brothers Grimm</label>
-                        </div>
-                        <div>
-                          <input type="radio" name="author" id="rowling" value="rowling" onChange={e => this.setState({ authorValue: e.target.value })}/>
-                          <label htmlFor="rowling">JK Rowling</label>
-                        </div>
-                        <div>
-                          <input type="radio" name="author" id="shakespeare" value="shakespeare" onChange={e => this.setState({ authorValue: e.target.value })}/>
-                          <label htmlFor="shakespeare">Shakespeare</label>
-                        </div>
-                        <button onClick={this.submitAuthor} type="submit">Submit</button>
-                    </div>
-                </TabPanel>
-            </Tabs>
             </div>
             </div>
-            </div>
-        </div>
+
         );
     }
 }
 export default App
+
+
+// { (this.state.isLoading) ?
+//       <div className="output-Box"><Error error={this.state.data} isLoading={this.state.isLoading}/></div> :
+//       <div className="output-Box"> <div className="output">{this.state.data}</div></div> }
+
+// <div className="output-Box">
+//   <div className="input-Box">
+//   <Tabs selectedindex={this.state.tabIndex} onSelect={tabIndex => this.setState({ tabIndex })}>
+//     <TabList selectedindex={this.state.tabIndex} onSelect={tabIndex => this.setState({ tabIndex })}>
+//         <Tab>text</Tab>
+//         <Tab>tweet</Tab>
+//         <Tab>url</Tab>
+//         <Tab>author</Tab>
+//     </TabList>
+//     <TabPanel>
+//     <div className="input-Box">
+//         <textarea className="input" placeholder='enter text here' value={this.state.inputTextValue}
+//             onChange={e => this.setState({ inputTextValue: e.target.value })}
+//             onSubmit={e => this.handleSubmit(e)}>
+//         </textarea>
+//         <button onClick={this.submitText} type="submit">Submit</button>
+//     </div>
+//     </TabPanel>
+//     <TabPanel>
+//     <div className="input-Box">
+//         <input className="inputUrl" value={this.state.handleValue}
+//             onChange={e => this.setState({ handleValue: e.target.value })}
+//             onSubmit={e => this.handleSubmit(e)}>
+//         </input>
+//         <button onClick={this.submitHandle} type="submit">Submit</button>
+//     </div>
+//     </TabPanel>
+//     <TabPanel>
+//         <div className="input-Box">
+//             <input className="inputUrl" value={this.state.urlValue}
+//                 onChange={e => this.setState({ urlValue: e.target.value })}
+//                 onSubmit={e => this.handleSubmit(e)}>
+//             </input>
+//             <button onClick={this.submitURL} type="submit">Submit</button>
+//         </div>
+//     </TabPanel>
+//     <TabPanel>
+//         <div className="input-Box">
+//             <div>
+//               <input type="radio" name="author" id="grimm" value="grimm" onChange={e => this.setState({ authorValue: e.target.value })}/>
+//               <label htmlFor="grimm">The Brothers Grimm</label>
+//             </div>
+//             <div>
+//               <input type="radio" name="author" id="rowling" value="rowling" onChange={e => this.setState({ authorValue: e.target.value })}/>
+//               <label htmlFor="rowling">JK Rowling</label>
+//             </div>
+//             <div>
+//               <input type="radio" name="author" id="shakespeare" value="shakespeare" onChange={e => this.setState({ authorValue: e.target.value })}/>
+//               <label htmlFor="shakespeare">Shakespeare</label>
+//             </div>
+//             <button onClick={this.submitAuthor} type="submit">Submit</button>
+//         </div>
+//     </TabPanel>
+// </Tabs>
+// </div>
+// </div>
--- src/Error.js
+++ src/Error.js
@@ -4,30 +4,13 @@ import './Error.css'
 
 
 function Error(props){
-    let content = undefined
-    // const urlGif = "https://media.giphy.com/media/feN0YJbVs0fwA/giphy.gif"
-    // const urlGif = "https://media.giphy.com/media/N256GFy1u6M6Y/giphy.gif"https://media.giphy.com/media/feN0YJbVs0fwA/giphy.gif
     const urlGif = "https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif";
-    if (props.error) {
-        content = (
-            <div className="Error">
-                <h1>hey{props.error.message}</h1>
-            </div>
-        )
-        } else {
-            content = (
-                <div className="Error">
-                hey
-                    {(props.isLoading) ? <img className="Loading" src={urlGif} alt="" />:""}
-                </div>
-            )
-        }
+    let content = (
+        <div className="Error">
+            { props.error ? <h1>{props.error.message}</h1> : props.isLoading ? <img className="Loading" src={urlGif} alt="" />:""}
+        </div>
+    );
     return content
 }
-
-// <iframe src="https://giphy.com/embed/feN0YJbVs0fwA" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/artists-on-tumblr-design-feN0YJbVs0fwA">via GIPHY</a></p>
-
-
-
 
 export default Error
